139e604c2c2c3f3dfa2125e3d9db39b6f4731479
moriahtao <moriahtao@ccs.neu.edu>
2018-03-02 15:55:04 -0500

    permit localhost

--- server/src/main/java/com/cs5500/server/SecurityConfig.java
+++ server/src/main/java/com/cs5500/server/SecurityConfig.java
@@ -84,8 +84,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8000");
-                //registry.addMapping("/**");
+                //registry.addMapping("/**").allowedOrigins(" http://localhost:8000");
+                registry.addMapping("/**");
 
             }
         };
