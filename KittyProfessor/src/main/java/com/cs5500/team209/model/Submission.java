b7e887d3df84179d937918ea1511ce75d20c365f
moriahtao <moriahtao@ccs.neu.edu>
2018-03-19 23:22:28 -0400

    CS209-36 #comment finish add new course

---
+++ KittyProfessor/src/main/java/com/cs5500/team209/model/Submission.java
@@ -0,0 +1,14 @@
+package com.cs5500.team209.model;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ * Created by mengtao on 3/19/18.
+ *
+ * Submission class for student
+ */
+public class Submission {
+    @Id
+    private String id;
+    
+}
---
+++ KittyProfessor/src/main/java/com/cs5500/team209/repository/AssignmentRepository.java
@@ -0,0 +1,24 @@
+package com.cs5500.team209.repository;
+
+import com.cs5500.team209.model.Assignment;
+import com.cs5500.team209.model.Course;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+/**
+ * Created by mengtao on 3/19/18.
+ */
+public interface AssignmentRepository extends MongoRepository<Assignment, String> {
+    /**
+     * find assignments by courseId
+     * @param courseId the courseId for query
+     * @return the assignments found
+     */
+    @Query("{'course.id' : ?0}")
+    List<Assignment> findAssignmentsForCourse(@Param("id") String courseId);
+}
